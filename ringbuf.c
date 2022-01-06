10d7164e0d402f44bc42cb40733c29251ef72dac
dine <dine@20hz.co.uk>
2022-01-06 23:10:04 +0000

    remove asserts

--- ringbuf.c
+++ ringbuf.c
@@ -74,7 +74,7 @@ ringbuf_reset(ringbuf_t rb)
 void
 ringbuf_free(ringbuf_t *rb)
 {
-    assert(rb && *rb);
+    // assert(rb && *rb);
     free((*rb)->buf);
     free(*rb);
     *rb = 0;
@@ -149,7 +149,7 @@ ringbuf_nextp(ringbuf_t rb, const uint8_t *p)
      * non-negative; therefore, the modulus operation is safe and
      * portable.
      */
-    assert((p >= rb->buf) && (p < ringbuf_end(rb)));
+    // assert((p >= rb->buf) && (p < ringbuf_end(rb)));
     return rb->buf + ((++p - rb->buf) % ringbuf_buffer_size(rb));
 }
 
@@ -163,7 +163,7 @@ ringbuf_findchr(const struct ringbuf_t *rb, int c, size_t offset)
 
     const uint8_t *start = rb->buf +
         (((rb->tail - rb->buf) + offset) % ringbuf_buffer_size(rb));
-    assert(bufend > start);
+    // assert(bufend > start);
     size_t n = MIN(bufend - start, bytes_used - offset);
     const uint8_t *found = memchr(start, c, n);
     if (found)
@@ -183,7 +183,7 @@ ringbuf_memset(ringbuf_t dst, int c, size_t len)
     while (nwritten != count) {
 
         /* don't copy beyond the end of the buffer */
-        assert(bufend > dst->head);
+        // assert(bufend > dst->head);
         size_t n = MIN(bufend - dst->head, count - nwritten);
         memset(dst->head, c, n);
         dst->head += n;
@@ -212,7 +212,7 @@ ringbuf_memcpy_into(ringbuf_t dst, const void *src, size_t count)
 
     while (nread != count) {
         /* don't copy beyond the end of the buffer */
-        assert(bufend > dst->head);
+        // assert(bufend > dst->head);
         size_t n = MIN(bufend - dst->head, count - nread);
         memcpy(dst->head, u8src + nread, n);
         dst->head += n;
@@ -270,7 +270,7 @@ ringbuf_memcpy_from(void *dst, ringbuf_t src, size_t count)
     const uint8_t *bufend = ringbuf_end(src);
     size_t nwritten = 0;
     while (nwritten != count) {
-        assert(bufend > src->tail);
+        // assert(bufend > src->tail);
         size_t n = MIN(bufend - src->tail, count - nwritten);
         memcpy(u8dst + nwritten, src->tail, n);
         src->tail += n;
@@ -281,7 +281,7 @@ ringbuf_memcpy_from(void *dst, ringbuf_t src, size_t count)
             src->tail = src->buf;
     }
 
-    assert(count + ringbuf_bytes_used(src) == bytes_used);
+    // assert(count + ringbuf_bytes_used(src) == bytes_used);
     return src->tail;
 }
 
@@ -322,9 +322,9 @@ ringbuf_copy(ringbuf_t dst, ringbuf_t src, size_t count)
     const uint8_t *dst_bufend = ringbuf_end(dst);
     size_t ncopied = 0;
     while (ncopied != count) {
-        assert(src_bufend > src->tail);
+        // assert(src_bufend > src->tail);
         size_t nsrc = MIN(src_bufend - src->tail, count - ncopied);
-        assert(dst_bufend > dst->head);
+        // assert(dst_bufend > dst->head);
         size_t n = MIN(dst_bufend - dst->head, nsrc);
         memcpy(dst->head, src->tail, n);
         src->tail += n;
@@ -338,7 +338,7 @@ ringbuf_copy(ringbuf_t dst, ringbuf_t src, size_t count)
             dst->head = dst->buf;
     }
 
-    assert(count + ringbuf_bytes_used(src) == src_bytes_used);
+    // assert(count + ringbuf_bytes_used(src) == src_bytes_used);
     
     if (overflow) {
         dst->tail = ringbuf_nextp(dst, dst->head);
